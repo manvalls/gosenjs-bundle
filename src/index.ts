d3ce54242757a152c1e4f53695329c7dc0e16fdb
Manuel Valls <me@manvalls.com>
2023-04-09 21:59:44 +0200

    Add gosen call

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { handlePopState, handleFormSubmit, handleLinkClick } from '@gosen/router'
+import { defineGosenCall } from '@gosen/call'
 
 handleLinkClick(window)
 handleFormSubmit(window)
 handlePopState({ window })
+
+defineGosenCall({ window })
